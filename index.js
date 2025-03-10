001537cd7094f7d955e3616d0f5aebea129267fc
Sabnock <kingsabnock@gmail.com>
2025-03-10 17:48:18 +0800

    Add CRUD operations for task management in Express app

--- index.js
+++ index.js
@@ -3,9 +3,49 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Express is working! Rafael B. Patiño")
+    res.json("Express is working! Rafael B. Patiño")
 });
 
 app.listen(port, () => {
     console.log("This express server listens to PORT 3000");
 })
+
+let tasks = [];
+let currentId = 1;
+
+app.use(express.json());
+
+app.post('/post', (req, res) => {
+    const { name, description } = req.body;
+    const newTask = { id: currentId++, name, description };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+});
+
+app.get('/read', (req, res) => {
+    res.json(tasks);
+});
+
+app.put('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    const task = tasks.find(t => t.id == id);
+    if (task) {
+        task.name = name;
+        task.description = description;
+        res.json(task);
+    } else {
+        res.status(404).send('Task not found');
+    }
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    const taskIndex = tasks.findIndex(t => t.id == id);
+    if (taskIndex !== -1) {
+        tasks.splice(taskIndex, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send('Task not found');
+    }
+});
